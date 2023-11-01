c162c6c1b2a96ebf098d8c81c77c60acb0aa088b
Rômulo F. Douro <rfdouro@gmail.com>
2023-11-01 14:05:00 -0300

    Cadastro de tarefas inicial

--- src/main/java/br/org/rfdouro/demoauth/controls/logica/TarefaController.java
+++ src/main/java/br/org/rfdouro/demoauth/controls/logica/TarefaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.org.rfdouro.demoauth.data.logica.Tarefa;
@@ -18,6 +19,11 @@ public class TarefaController {
  @Autowired
  TarefaRepository tarefaRepository;
 
+ @GetMapping("/cadastro")
+ public String cadastro() {
+  return "tarefa/cadastro";
+ }
+
  @GetMapping
  public String index(Model model) {
   model.addAttribute("listaTarefas", tarefaRepository.findAll());
@@ -25,8 +31,16 @@ public class TarefaController {
  }
 
  @PostMapping
- public String postMethodName(@RequestBody Tarefa tarefa) {
-  tarefaRepository.save(tarefa); 
+ public String cadastrar(String descricao) {
+  Tarefa tarefa = new Tarefa();
+  tarefa.setDescricao(descricao);
+  tarefaRepository.save(tarefa);
+  return "redirect:/tarefa";
+ }
+
+ @GetMapping("/excluir/{id}")
+ public String excluir(@PathVariable(name = "id", required = true) Long id) {
+  tarefaRepository.deleteById(id);
   return "redirect:/tarefa";
  }
 
