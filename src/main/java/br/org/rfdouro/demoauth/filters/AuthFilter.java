ac5283c68b65840d04e1c15c9f2b30646c5ae085
Rômulo F. Douro <rfdouro@gmail.com>
2023-11-16 13:40:17 -0300

    atualizacao -> filtro com component apenas -> context e thymeleaf para tomcat

--- src/main/java/br/org/rfdouro/demoauth/filters/AuthFilter.java
+++ src/main/java/br/org/rfdouro/demoauth/filters/AuthFilter.java
@@ -20,64 +20,69 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
 
-@Component
-@WebFilter(urlPatterns = "/*")
+@Service
+//@WebFilter(urlPatterns = "/*")
 public class AuthFilter implements Filter {
 
-  @Autowired
-  AutorizacaoRepository autorizacaoRepository;
+ @Autowired
+ AutorizacaoRepository autorizacaoRepository;
 
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+ @Override
+ public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+         throws IOException, ServletException {
 
-    String url = request instanceof HttpServletRequest ? ((HttpServletRequest) request).getRequestURL().toString()
-        : "N/A";
+  String context = request.getServletContext().getContextPath();
 
-    try {
-      URL _url = new URI(url).toURL();
-      url = _url.getFile();
-    } catch (URISyntaxException e) {
-      e.printStackTrace();
-    }
+  String url = request instanceof HttpServletRequest ? ((HttpServletRequest) request).getRequestURL().toString()
+          : "N/A";
 
-    System.out.println("processando a url: " + url);
+  try {
+   URL _url = new URI(url).toURL();
+   url = _url.getFile();
+   url = url.replace(context, "");
+  } catch (URISyntaxException e) {
+   e.printStackTrace();
+  }
 
-    var usuLogado = request instanceof HttpServletRequest
-        ? ((HttpServletRequest) request).getSession().getAttribute("usuario")
-        : null;
+  System.out.println("processando a url: " + url);
 
-    System.out.println("usuário logado : " + usuLogado);
+  var usuLogado = request instanceof HttpServletRequest
+          ? ((HttpServletRequest) request).getSession().getAttribute("usuario")
+          : null;
 
-    // se não precisar de permissão segue em frente
-    if (!autorizacaoRepository.precisaVerificar(url)) {
+  System.out.println("usuário logado : " + usuLogado);
+
+  // se não precisar de permissão segue em frente
+  if (!autorizacaoRepository.precisaVerificar(url)) {
+   chain.doFilter(request, response);
+   return;
+  }
+
+  // se precisa de permissão verifica se tem usuário logado
+  if (usuLogado != null) {
+   Usuario u = (Usuario) usuLogado;
+   if (u.getAutorizacoes() != null) {
+    for (Autorizacao a : u.getAutorizacoes()) {
+     System.out.println(a.getPattern());
+
+     Pattern pattern = Pattern.compile(a.getPattern(), Pattern.UNICODE_CASE);
+     Matcher matcher = pattern.matcher(url);
+     boolean matchFound = matcher.find();
+     if (matchFound) {
+      System.out.println("ok aqui");
       chain.doFilter(request, response);
       return;
+     }
     }
-
-    // se precisa de permissão verifica se tem usuário logado
-    if (usuLogado != null) {
-      Usuario u = (Usuario) usuLogado;
-      if (u.getAutorizacoes() != null)
-        for (Autorizacao a : u.getAutorizacoes()) {
-          System.out.println(a.getPattern());
-
-          Pattern pattern = Pattern.compile(a.getPattern(), Pattern.UNICODE_CASE);
-          Matcher matcher = pattern.matcher(url);
-          boolean matchFound = matcher.find();
-          if (matchFound) {
-            System.out.println("ok aqui");
-            chain.doFilter(request, response);
-            return;
-          }
-        }
-      request.getRequestDispatcher("/naoautorizado").forward(request, response);
-      return; // recusa a navegação e direciona para a página naoautorizado
-    } else {
-      request.getRequestDispatcher("/login").forward(request, response);
-      return; // direciona para o login
-    }
+   }
+   request.getRequestDispatcher("/naoautorizado").forward(request, response);
+   return; // recusa a navegação e direciona para a página naoautorizado
+  } else {
+   request.getRequestDispatcher("/login").forward(request, response);
+   return; // direciona para o login
   }
+ }
 
 }
